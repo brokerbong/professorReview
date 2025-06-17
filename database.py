5880115249276f3d4b283426d4c62be442221dda
brokerbong <86176538+brokerbong@users.noreply.github.com>
2025-06-18 00:05:03 +0900

    Update database.py

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from psycopg2.pool import SimpleConnectionPool
 import os
 
 # 환경 변수 또는 하드코딩 가능 (보안상 환경 변수 권장)
-DB_HOST = "c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com"
+DB_HOST = ""
 DB_PORT = 5432
-DB_NAME = "d2m7pqo5ucoj9p"
-DB_USER = "ubhn5jf7o0a2rl"
-DB_PASSWORD = "p7f603737160f86855834796b40c3a7c1c47e04712922052ae002d47ea8c95df1"
+DB_NAME = ""
+DB_USER = ""
+DB_PASSWORD = ""
 
 # 커넥션 풀 생성
 db_pool = SimpleConnectionPool(
