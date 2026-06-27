24519234045f2b255bd3baa20ff5d9d9f12508cf
brokerbong <86176538+brokerbong@users.noreply.github.com>
2026-06-27 17:39:18 +0900

    Create oracle_sangchu_db.py

---
+++ oracle_sangchu_db.py
@@ -0,0 +1,28 @@
+import os
+import oracledb
+
+ORACLE_USER = os.getenv("ORACLE_USER")
+ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
+ORACLE_DSN = os.getenv("ORACLE_DSN")
+
+oracle_pool = None
+
+
+def get_oracle_pool():
+    global oracle_pool
+
+    if oracle_pool is None:
+        oracle_pool = oracledb.create_pool(
+            user=ORACLE_USER,
+            password=ORACLE_PASSWORD,
+            dsn=ORACLE_DSN,
+            min=1,
+            max=5,
+            increment=1
+        )
+
+    return oracle_pool
+
+
+def get_sangchu_conn():
+    return get_oracle_pool().acquire()
